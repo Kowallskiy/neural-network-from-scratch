80c547dc6ed28fac8eaf7f96dde4dadd4ff74659
User <trane-101@yandex.ru>
2023-10-30 22:12:30 +0400

    added Adam optimizer

--- f.py
+++ f.py
@@ -58,3 +58,43 @@ class Activation_Softmax:
             single_output = single_output.reshape(-1, 1)
             jacobian_matrix = np.diagflat(single_output) + np.dot(single_output, single_output.T)
             self.dinputs[index] = np.dot(jacobian_matrix, single_dvalue)
+
+class Adam_Optimizer:
+
+    def __init__(self, epsilon=1e-7, lr= 1e-5, decay=0, beta_1=0.9, beta_2=0.999):
+        self.learning_rate =lr
+        self.current_learning_rate = lr
+        self.decay = decay
+        self.epsilon = epsilon
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.iterations = 0
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1 / (1 + self.decay * self.iterations))
+
+    def update_params(self, layer):
+        if not hasattr(layer, 'weight_momentums'):
+            layer.weight_momentums = np.zeros_like(layer.weights)
+            layer.bias_momentums = np.zeros_like(layer.biases)
+            layer.weights_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        layer.weight_momentums = self.beta_1 * layer.weight_momentums + (1 - self.beta_1) * layer.dweights
+        layer.bias_momentums = self.beta_1 * layer.bias_momentums + (1 - self.beta_1) * layer.dbiases
+
+        weight_momentums_corrected = layer.weight_momentums / (1 - self.beta_1**(self.iterations + 1))
+        bias_momentums_corrected = layer.bias_momentums / (1 - self.beta_1**(self.iterations + 1))
+
+        layer.weight_cache = self.beta_2 * layer.weight_cache + (1 - self.beta_2) * layer.dweights ** 2
+        layer.bias_cache = self.beta_2 * layer.bias_cache + (1 - self.beta_2) * layer.dbiases ** 2
+
+        weights_cache_corrected = layer.weight_cache / (1 - self.beta_2**(self.iterations + 1))
+        bias_cache_corrected = layer.bias_cache / (1 - self.beta_2**(self.iterations + 1))
+
+        layer.weights += -self.current_learning_rate * weight_momentums_corrected / (np.sqrt(weights_cache_corrected) + self.epsilon)
+        layer.biases += -self.current_learning_rate * bias_momentums_corrected / (np.sqrt(bias_cache_corrected) + self.epsilon)
+
+    def post_update_params(self):
+        self.iterations += 1
